01aac3e98ffec87c917c028beb2aeecd6285feb5
P <78220050+Pgtgt@users.noreply.github.com>
2021-10-12 22:49:18 +0900

    add theta analysis

--- k推測.py
+++ k推測.py
@@ -11,6 +11,31 @@ import os
 from scipy import optimize
 import matplotlib.pyplot as plt
 import sys
+from PyQt5 import QtWidgets
+import ref_index
+
+# =============================================================================
+# PARAM SETTING (n_air(refractive index) m_per_gr(grating m/groove), M(order))
+# =============================================================================
+n_air = ref_index.edlen(wave=(1554.134049+1563.862587)/2, t=27, p=101325, rh=70)
+
+# Grating info
+# https://www.thorlabs.co.jp/thorproduct.cfm?partnumber=GR13-0616
+gr_per_mm = 600  # grating pitch /mm
+mm_per_gr = 1 / gr_per_mm
+m_per_gr = mm_per_gr * 1e-3
+
+M = 1
+c = 299792458
+
+
+# =============================================================================
+# csvmatome.xlxsから，データを取り出し，
+# 強度:df_intensities["ファイル名"]
+# frequency:freq
+# =============================================================================
+app = QtWidgets.QApplication(sys.argv)
+
 
 def Dialog_File(rootpath=r"C:", caption="choise"):
     """
@@ -23,76 +48,83 @@ def Dialog_File(rootpath=r"C:", caption="choise"):
     Returns:
         folderpath (str): folder path.
     """
-    from PyQt5 import QtWidgets
+
     # ディレクトリ選択ダイアログを表示-
-    app = QtWidgets.QApplication(sys.argv)
     filepath = QtWidgets.QFileDialog.getOpenFileName(
         parent=None, caption=caption, directory=rootpath)
 
     return filepath[0]
 
-# =============================================================================
-# csvmatome.xlxsから，データを取り出し，
-# 強度:df_intensities["ファイル名"]
-# frequency:freq
-# =============================================================================
 
-path_csvmatome = Dialog_File(caption = "choose matome.xlxs")
-df_wholedata = pd.read_excel(path_csvmatome, sheet_name = "wholedata",header =0, index_col =0)
-df_sort = pd.read_excel(path_csvmatome, sheet_name = "sort",header =0, index_col =0)
+path_csvmatome = Dialog_File(caption="choose matome.xlxs")
+df_wholedata = pd.read_excel(
+    path_csvmatome, sheet_name="wholedata", header=0, index_col=0)
+df_sort = pd.read_excel(path_csvmatome, sheet_name="sort", header=0, index_col=0)
 
-df_intensities=df_wholedata.loc["[TRACE DATA]":].iloc[1:].astype(float)*1e-3
-freq=df_wholedata.loc["[TRACE DATA]":].iloc[1:].astype(float).index *1e12
+df_intensities = df_wholedata.loc["[TRACE DATA]":].iloc[1:].astype(float)*1e-3
+freq = df_wholedata.loc["[TRACE DATA]":].iloc[1:].astype(float).index * 1e12
 
 # name0 = "000060-0_OSA1@-10000pls"
 
 # =============================================================================
 # intensity = A * norm(sigma, mu)形式のフィッテイングを行う．
 # A, sigma, muを返す．
+# PARAM SETTINGSの値を使って，　回折格子方程式からtheta(:中心周波数)を出す．
 # https://lastline.hatenablog.com/entry/2018/04/12/112502
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
 # =============================================================================
 
 # 推定後パラメータを格納用のdfを設定
-df_fit = pd.DataFrame(index = ["A","mu","sigma","r2"], columns =df_intensities.columns)
+df_fit = pd.DataFrame(index=["A", "mu:f_center", "theta_rad", "sigma",
+                      "r2", "n_air", "d(m/groove)"], columns=df_intensities.columns)
+
+
 
 # 関数定義
 def gauss(x, A=1, mu=0, sigma=1):
     return A * np.exp(-(x - mu)**2 / (2*sigma**2))
 
+
 for dataname in df_intensities.columns:
 
     # 関数のピークから推定用の初期値を設定
-    peak_num=np.argmax(df_intensities[dataname])
+    peak_num = np.argmax(df_intensities[dataname])
     init_param = [df_intensities[dataname][peak_num], freq[peak_num], 0.1e+12]
-
+    print("\r"+dataname, end="")
     # 最適化
-    popt = 0,0,0 # default
     try:
-        popt, pcov = optimize.curve_fit(gauss, freq, df_intensities[dataname], p0 = init_param)
+        # Gaussian fitting
+        popt, pcov = optimize.curve_fit(
+            gauss, freq, df_intensities[dataname], p0=init_param)
+
+        # f_center by Grating equ.
+        f_center = popt[1]
+        theta_rad = np.arcsin(M*c / (n_air*f_center*m_per_gr))
+
         # https://sabopy.com/py/curve_fit/
-        residuals =  df_intensities[dataname] - gauss(freq, popt[0],popt[1],popt[2])
-        rss = np.sum(residuals**2)#residual sum of squares = rss
-        tss = np.sum((df_intensities[dataname]-np.mean(df_intensities[dataname]))**2)#total sum of squares = tss
+        # 参考までにR2値を出す
+        residuals = df_intensities[dataname] - gauss(freq, popt[0], popt[1], popt[2])
+        rss = np.sum(residuals**2)  # residual sum of squares = rss
+        # total sum of squares = tss
+        tss = np.sum((df_intensities[dataname]-np.mean(df_intensities[dataname]))**2)
         r2 = 1 - (rss / tss)
-        df_fit.loc["A",dataname],df_fit.loc["mu",dataname],df_fit.loc["sigma",dataname]=popt
-        df_fit.loc["r2",dataname] = r2
+        #　結果をdf_fitへ格納
+        df_fit.loc["A", dataname], df_fit.loc["mu:f_center",
+                                              dataname], df_fit.loc["sigma", dataname] = popt
+        df_fit.loc["theta_rad", dataname] = theta_rad
+        df_fit.loc["r2", dataname] = r2
     except:
         print(dataname+"failure")
 
 df_sort_fit = pd.concat([df_sort, df_fit])
 
 
-with pd.ExcelWriter(path_csvmatome, engine="openpyxl",mode = "a") as writer:
-    #engine="openpyxl"にしないと，mode = "a"が使えない
+with pd.ExcelWriter(path_csvmatome, engine="openpyxl", mode="a") as writer:
+    # engine="openpyxl"にしないと，mode = "a"が使えない
     # https://stackoverflow.com/questions/54863238/pandas-excelwriter-valueerror-append-mode-is-not-supported-with-xlsxwriter
     df_sort_fit.to_excel(writer, sheet_name="sort_fit",)
-
-
-
 
 
 # col = ["s1","s2","s3","s4","s5",]
 # index =[1,2,3,4,5,6,7,8]
 # df = pd.DataFrame(index = index, columns = col)
-
