57e28f7445731f8e932eceaf848b4fb46131b40a
P <78220050+Pgtgt@users.noreply.github.com>
2021-10-19 09:51:15 +0900

    Update sampleヒルベルト変換の資料.py

--- 卒論時作成/sampleヒルベルト変換の資料.py
+++ 卒論時作成/sampleヒルベルト変換の資料.py
@@ -13,13 +13,17 @@ time = numpy.arange(0., T, 1 / fs)
 frequency = 1000
 noise = numpy.random.normal(0, 0.3, int(fs/10))
 signal = 4+numpy.sin(2 * numpy.pi * frequency * time)*numpy.cos(2 * numpy.pi * frequency/500 * time)
-analyticSignal = hilbert(signal-4)
+signal_center = signal-4
+analyticSignal = hilbert(signal_center)
 amplitudeEvelope = numpy.abs(analyticSignal)
-cutoff =1000
-filteredSignal = FilteredSignal(amplitudeEvelope+4, fs, cutoff)
+cutoff =500
+filteredSignal = FilteredSignal(amplitudeEvelope, fs, cutoff)
 
 fig2, ax2 = plt.subplots(1, 1)
-ax2.plot(time, signal)
+ax2.plot(time, signal_center)
+
+ax2.plot(time, amplitudeEvelope)
+
 ax2.plot(time, filteredSignal)
 ax2.set_xlabel('Tiempo')
 ax2.set_ylabel('Amplitud')
