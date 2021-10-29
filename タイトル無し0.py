3beb364bc56b23d29f23d73396e8797a5ac7d84f
P <78220050+Pgtgt@users.noreply.github.com>
2021-10-29 22:11:12 +0900

    Update タイトル無し0.py

--- タイトル無し0.py
+++ タイトル無し0.py
@@ -1,29 +1,1871 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 21 11:38:10 2021
+Created on Fri Oct 29 19:24:24 2021
 
-@author: anonymous
+@author: wsxhi
 """
 
-import pandas as pd
-import numpy as np
-import os
-import math
-import sys
-import copy
-from icecream import ic
-# import copy
-import sklearn.metrics as metrics
-import matplotlib.pyplot as plt
-import ref_index
-from scipy.fftpack import fft, fftfreq
+print(2)
+print(4)
 
+print(8
+      )
 
-def zero_padding(data, len_pad):
-    pad = np.zeros(len_pad)
-    data_pad = np.insert(pad, 0, data)
-    acf = (sum(np.abs(data)) / len(data)) / (sum(np.abs(data_pad)) / len(data_pad))
-    return data_pad * acf
+print(4)
 
+print(2)
+print(4)
 
-print(5)
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+
+print(2)
+print(4)
+
+print(8
+      )
+
+print(4)
+print("fin")
