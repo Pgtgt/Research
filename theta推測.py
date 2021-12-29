9c9ee904a33648c35a09bb5153157360d1d6fbfa
P <78220050+Pgtgt@users.noreply.github.com>
2021-12-29 19:22:52 +0900

    Update theta推測.py
    fcenters =>fcenter=>theta=>kの流れを正しく再現した

--- theta推測.py
+++ theta推測.py
@@ -10,7 +10,7 @@ Created on Sat Oct  9 20:59:13 2021
 等を求める．
 その後csvmaotme.xlsxにsheet_name = "sort_fit"を追加
 
-* %matplotlib inlineを奨励
+! %matplotlib inlineを奨励
 連続してplt.show()するため，
 %matplotlib により実行すると固まる
 
@@ -25,10 +25,13 @@ import sys
 from PyQt5 import QtWidgets
 import ref_index
 
-# =============================================================================
-# PARAM SETTING (n_air(refractive index) m_per_gr(grating m/groove), M(order))
-# =============================================================================
-n_air = ref_index.edlen(wave=(1554.134049+1563.862587)/2, t=27, p=101325, rh=70)
+# *************************
+# * PARAM SETTING (n_air(refractive index) m_per_gr(grating m/groove), M(order))
+# *************************
+
+dict_nparam = dict(wave =  (1554.134049+1563.862587)/2,
+               t=27, p=101325,rh=70)
+n_air = ref_index.edlen(wave=dict_nparam["wave"], t=dict_nparam["t"], p=dict_nparam["p"], rh=dict_nparam["rh"])
 
 # Grating info
 # https://www.thorlabs.co.jp/thorproduct.cfm?partnumber=GR13-0616
@@ -39,15 +42,12 @@ m_per_gr = mm_per_gr * 1e-3
 M = 1
 c = 299792458
 
-
-
-# =============================================================================
-# csvmatome.xlxsから，データを取り出し，
-# 強度:df_intensities["ファイル名"]
-# frequency:freq
-# =============================================================================
+# **************************
+# * csvmatome.xlxsから，データを取り出し，
+# * 強度:df_intensities["ファイル名"]
+# * frequency:freq
+# **************************
 app = QtWidgets.QApplication(sys.argv)
-
 
 def Dialog_File(rootpath=r"C:", caption="choise"):
     """
@@ -77,27 +77,43 @@ df_intensities = df_wholedata.loc["[TRACE DATA]":].iloc[1:].astype(float)*1e-3
 freq = df_wholedata.loc["[TRACE DATA]":].iloc[1:].astype(float).index * 1e12
 
 
-# =============================================================================
-# intensity = A * norm(sigma, mu)形式のフィッテイングを行う．
-# A, sigma, muを返す．
-# PARAM SETTINGSの値を使って，　回折格子方程式からtheta(:中心周波数)を出す．
-# https://lastline.hatenablog.com/entry/2018/04/12/112502
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
-# =============================================================================
+# **************************
+# * intensity = A * norm(sigma, mu)形式のフィッテイングを行う．
+# * A, sigma, muを返す．
+# * https://lastline.hatenablog.com/entry/2018/04/12/112502
+# * https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
+# **************************
 
-"""推定後パラメータを格納用のdfを設定"""
-df_fit = pd.DataFrame(index=["k","A", "mu:f_center", "theta_rad", "sigma",
-                      "r2", "n_air", "d(m/groove)"], columns=df_intensities.columns)
+# gfdsg
+# index=["k","A", "mu:f_center", "theta_rad", "sigma","r2", "n_air", "d(m/groove)"]
+
+df_fit = pd.DataFrame(index=["A", "mu:f_center", "sigma","trust","f_center","theta_rad","theta_deg","k","d(m/groove)","n_air","wave","t","p","rh"],
+                      columns=df_intensities.columns)
+"""[推定後パラメータを格納用のdf_fitを設定]"""
 
 
-"""関数定義"""
 def gauss(x, A=1, mu=0, sigma=1):
-    return A * np.exp(-(x - mu)**2 / (2*sigma**2))
+    """[summary]
 
+    Args:
+        x ([float array]): [description]
+        A (int, optional): [description]. Defaults to 1.
+        mu (int, optional): [description]. Defaults to 0.
+        sigma (int, optional): [description]. Defaults to 1.
+
+    Returns:
+        y [float array]: [description]
+    """    
+    y=A * np.exp(-(x - mu)**2 / (2*sigma**2))
+    return y
+
+
+
+max_intensity = df_intensities.max().max()
+"""[全強度中の最大強度]"""
 
 for dataname in df_intensities.columns:
-
-    """関数のピークから推定用の初期値を設定"""
+    """関数のピークから推定用の初期値"init_param"を設定"""
     peak_num = np.argmax(df_intensities[dataname])
     init_param = [df_intensities[dataname][peak_num], freq[peak_num], 0.1e+12]
 
@@ -109,40 +125,59 @@ for dataname in df_intensities.columns:
         popt, pcov = optimize.curve_fit(
             gauss, freq, df_intensities[dataname], p0=init_param)
 
-
-        """f_center by Grating equ."""
-        f_center = popt[1]
-        theta_rad = np.arcsin(M*c / (n_air*f_center*m_per_gr))
-
-        """R2値https://sabopy.com/py/curve_fit/"""
-        residuals = df_intensities[dataname] - gauss(freq, popt[0], popt[1], popt[2])
-        rss = np.sum(residuals**2)  # residual sum of squares = rss
-        # total sum of squares = tss
-        tss = np.sum((df_intensities[dataname]-np.mean(df_intensities[dataname]))**2)
-        r2 = 1 - (rss / tss)
-
-        """k"""
-        k = 1/ (n_air * (1 + np.cos(theta_rad)))
-
-        """結果をdf_fitへ格納"""
-        df_fit.loc["k", dataname] = k
+        """Store Results"""
         df_fit.loc["A", dataname], df_fit.loc["mu:f_center",
-                                              dataname], df_fit.loc["sigma", dataname] = popt
-        df_fit.loc["theta_rad", dataname] = theta_rad
-        df_fit.loc["r2", dataname] = r2
+                                        dataname], df_fit.loc["sigma", dataname] = popt
+        
+        # """R2値https://sabopy.com/py/curve_fit/"""
+        # residuals = df_intensities[dataname] - gauss(freq, popt[0], popt[1], popt[2])
+        # rss = np.sum(residuals**2)  # residual sum of squares = rss
+        # # total sum of squares = tss
+        # tss = np.sum((df_intensities[dataname]-np.mean(df_intensities[dataname]))**2)
+        # r2 = 1 - (rss / tss)
 
-        """plot (%matplotlib inlineがおすすめ)"""
+        # ! plot (%matplotlib inlineがおすすめ)
         plt.plot(freq, df_intensities[dataname])
         plt.plot(freq, gauss(freq, A=popt[0], mu=popt[1], sigma=popt[2]))
         plt.title(dataname)
-        plt.ylim( -0.01*df_intensities.max().max(), df_intensities.max().max())
+        plt.ylim( -0.01*max_intensity, max_intensity)
         plt.show()
-
+        
     except:
         print(dataname+"failure")
+        
+# **************************
+# * 信頼区間の選別とf_center導出．
+# * 信頼区間を求め，具体的区間span_trustの表示
+# * 信頼区間から各パラメータ(f_center, theta_Rad, k)等を算出 
+# * df_sort_fit = df_sort + df_fitを足して，データ保存
+# **************************
+
+# 1/2*max_in(強度不足)  <A <  1.5*max_in（外れ値）]
+span_trust=((max_intensity/2 < df_fit.loc["A",:]) & (df_fit.loc["A",:]<max_intensity*1.5))
+
+df_fit.loc["trust", :]=span_trust
+f_center = df_fit.loc["mu:f_center", span_trust].mean()
+theta_rad = np.arcsin(M*c/(n_air*f_center*m_per_gr) )
+theta_deg = np.degrees(theta_rad)
+k = 1/ (n_air * (1 + np.cos(theta_rad)))
+
+"""結果および屈折率関係をdf_fitへ格納 最初の列のみに格納"""
+df_fit.loc["f_center", df_intensities.columns[0]] = f_center
+df_fit.loc["theta_rad", df_intensities.columns[0]] = theta_rad
+df_fit.loc["theta_deg", df_intensities.columns[0]] = theta_deg
+
+df_fit.loc["k", df_intensities.columns[0]] = k
+df_fit.loc["d(m/groove)", df_intensities.columns[0]] = m_per_gr
+
 
 df_fit.loc["n_air", df_intensities.columns[0]] = n_air
-df_fit.loc["d(m/groove)", df_intensities.columns[0]] = m_per_gr
+df_fit.loc["wave", df_intensities.columns[0]] = dict_nparam["wave"]
+df_fit.loc["t", df_intensities.columns[0]] = dict_nparam["t"]
+df_fit.loc["p", df_intensities.columns[0]] = dict_nparam["p"]
+df_fit.loc["rh", df_intensities.columns[0]] = dict_nparam["rh"]
+
+
 df_sort_fit = pd.concat([df_sort, df_fit])
 
 
